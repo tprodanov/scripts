49356a61eddf7c4aac2e3eb77daca5cadfe6eaa2
Timofey Prodanov <timofey.prodanov@hhu.de>
2026-04-17 09:09:01 +0200

    New script combining subseq-agc and map-to-agc

---
+++ pangenie-polish/map-and-subseq.sh
@@ -0,0 +1,175 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+shopt -s nullglob
+
+readonly SCRIPT_NAME="$(basename "${BASH_SOURCE[0]:-$0}")"
+
+function help_message {
+  cat <<HELP
+Usage: $SCRIPT_NAME (-a FILE | -g DIR) -t FILE -o DIR [-d INT] [-- minimap-args]
+
+Maps target sequences to assembly genomes and extracts corresponding subregions.
+
+Available options:
+    -a, --agc      FILE  Input AGC file.
+    -g, --genomes  DIR   Directory with various genome assemblies (.fa[.gz]).
+                         Mutually exclusive with -a/--agc.
+    -t, --targets  FILE  FASTA file with target sequences. Name lines should not contain spaces.
+    -o, --output   DIR   Output directory.
+    -d, --distance INT   Merge PAF entries if distance is smaller than INT [${distance}].
+    -h, --help           Print this help and exit.
+
+Provide minimap2 arguments after --
+    Default arguments are "-cx asm20 -t 3 -N 10 -p 0.5"
+HELP
+}
+
+function setup_colors {
+    readonly RED="\e[31m"
+    readonly ENDCOLOR="\e[0m"
+}
+
+function msg {
+    echo -e "$*" >&2
+}
+
+function err {
+    msg "${RED}[ERROR]${ENDCOLOR} $*"
+}
+
+function panic {
+    err "$1"
+    exit "${2-1}" # Return 1 by default.
+}
+
+function parse_params {
+    distance=5000
+
+    ARGS="$(getopt -o a:g:t:o:d:h --long agc:,genomes:targets:,output:,distance:,help \
+        --name "$SCRIPT_NAME" -- "$@")"
+    eval set -- "$ARGS"
+    while :; do
+        case "$1" in
+            -a | --agc )
+                agc_file="$2"; shift 2 ;;
+            -g | --genomes )
+                genomes_dir="$2"; shift 2 ;;
+            -t | --targets )
+                targets_file="$2"; shift 2 ;;
+            -o | --output )
+                output="$2"; shift 2 ;;
+            -d | --distance )
+                distance="$2"; shift 2 ;;
+            -h | --help)
+                help_message; exit 0;
+                ;;
+            -- ) shift; break ;;
+            * )  panic "Unexpected argument $1" ;;
+        esac
+    done
+
+    if [[ ${#@} -ne 0 ]]; then
+        minimap2_args=( "$@" )
+    else
+        minimap2_args=( -cx asm20 -t 3 -N 10 -p 0.5 )
+    fi
+
+    [[ ! -z "${targets_file-}" ]] || panic "Missing required parameter -t/--targets"
+    [[ ! -z "${output-}" ]]   || panic "Missing required parameter -o/--output"
+
+    [[ -z "${agc_file-}" ]] && have_agc=n || have_agc=y
+    [[ -z "${genomes_dir-}" ]] && have_genomes=n || have_genomes=y
+    [[ $have_agc != $have_genomes ]] || panic "Require either -a or -g, but not both"
+}
+
+function process_genome {
+    local arg="$1"
+    local genome_name
+    [[ $have_agc = y ]] && genome_name="$arg" || genome_name="$(basename "${arg%.fa*}")"
+
+    local prefix="${output}/${genome_name}"
+    local ok_file="${prefix}.ok"
+    local lock_file="${prefix}.lock"
+    if [[ -f "$ok_file" ]]; then
+        return
+    fi
+    if ! ( set -C; 2>/dev/null > "$lock_file" ); then
+        return
+    fi
+    trap 'rm -f "${lock_file}"; exit 1' INT TERM ERR
+
+    # ===== START ======
+    msg "Processing $genome_name"
+    mkdir -p "$prefix"
+
+    local genome_fasta
+    if [[ $have_agc = y ]]; then
+        genome_fasta="${prefix}.fa"
+        agc getset "$agc_file" "$genome_name" > "${genome_fasta}"
+        samtools faidx "${genome_fasta}"
+    else
+        genome_fasta="$arg"
+    fi
+
+    local paf_filename="${prefix}.paf.gz"
+    if [[ ! -f "$paf_filename" ]]; then
+        msg "    Mapping targets to assembly"
+        minimap2 "${minimap2_args[@]}" "$genome_fasta" "$targets_file" 2> /dev/null | \
+            gzip > "${paf_filename}.tmp" \
+            && mv "${paf_filename}"{.tmp,}
+    fi
+
+    msg "    Extracting subsequences"
+    for target in "${target_names[@]}"; do
+        # First, take PAF for given target and convert to BED file, then sort and merge.
+        # Then, sort it and merge;
+        zcat "${paf_filename}" | \
+            awk -F$'\t' -v target="$target" \
+                'BEGIN{OFS=FS} $1 == target { print $6,$8,$9,target }' | \
+            sort -k1,1V -k2,2n | \
+            bedtools merge -d "$distance" -c 4 -o distinct \
+            > "${prefix}/${target}.bed"
+
+        # Select largest region and convert it into "chr:start-end" format.
+        local region
+        region="$(awk -F$'\t' 'BEGIN{OFS=FS} {
+                if ($3 - $2 > len) {
+                    len = $3 - $2;
+                    region = ($1 ":" ($2+1) "-" $3);
+                }
+            } END { print region }' "${prefix}/${target}.bed")"
+
+        # If there is a region, extract it
+        if [[ ! -z "$region" ]]; then
+            samtools faidx "$genome_fasta" "$region" | gzip > "${prefix}/${target}.fa.gz"
+        fi
+    done
+
+    [[ $have_agc = n ]] || rm "${genome_fasta}"{,.fai}
+
+    cat "${prefix}"/*.bed | sort -k1,1V -k2,2n | gzip > "${prefix}.bed.gz"
+    rm "${prefix}"/*.bed
+    # ===== END ======
+
+    touch "${ok_file}"
+    rm -f "${lock_file}"
+    trap - INT TERM ERR
+}
+
+setup_colors
+parse_params "$@"
+
+# zcat -f opens plain files as well. sed -n does not print by default.
+readarray -t target_names < <(zcat -f "$targets_file" | sed -n 's/>//p' | sort -u)
+
+mkdir -p "$output"
+if [[ $have_agc = y ]]; then
+    agc listset "$agc_file" | while read genome; do
+        process_genome "$genome"
+    done
+else
+    for genome in "${genomes_dir}"/*.fa{,sta}{,.gz}; do
+        process_genome "$genome"
+    done
+fi
